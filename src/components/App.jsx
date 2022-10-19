0d92439eb2c0fbce3019f858a5b611b3ad54696b
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-10-19 14:06:32 +0300

    Add start settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,16 @@
-import { useState, useEffect, useContext } from 'react';
-import { EventsList } from './EventsList/EventsList';
-import { fetchApi } from 'fetchApi/fetchApi';
-import { AuthContext } from 'authContext/AuthContext';
-import { AuthNav } from './AuthNav/AuthNav';
-
 export const App = () => {
-  const [events, setEvents] = useState([]);
-  const [isShown, setIsShown] = useState(false);
-  const [page, setPage] = useState(1);
-
-  const { isAuth } = useContext(AuthContext);
-
-  useEffect(() => {
-    if (isShown) {
-      fetchApi(page).then(resp =>
-        setEvents(prevEvents => [...prevEvents, ...resp.data._embedded.events])
-      );
-    }
-  }, [isShown, page]);
-
-  const showEvents = () => {
-    setIsShown(true);
-  };
-
-  const loadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  const deleteEvent = eventId => {
-    setEvents(prevEvents => prevEvents.filter(event => event.id !== eventId));
-  };
-
   return (
-    <>
-      <AuthNav />
-      {isShown && <EventsList events={events} deleteEvent={deleteEvent} />}
-      {!isAuth && <p>You need authorization</p>}
-      {!isShown && isAuth && (
-        <button type="button" onClick={showEvents}>
-          Show events
-        </button>
-      )}
-      {isShown && (
-        <button type="button" onClick={loadMore}>
-          Load more
-        </button>
-      )}
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { AuthContextProvider } from 'authContext/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <App />
   </React.StrictMode>
 );
